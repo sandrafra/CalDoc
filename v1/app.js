72e2a1a92770dbda484e48002d8fe04989692082
sandrafra <s.k.fraczek@gmail.com>
2020-06-19 19:47:29 +0200

    Add login dependent on the type of profile and add authorization

--- v1/app.js
+++ v1/app.js
@@ -3,8 +3,11 @@ var express = require("express"),
     session = require("express-session"),
     bodyParser = require("body-parser"),
     mysql = require("mysql"),
-    bcrypt = require("bcrypt");
+    bcrypt = require("bcrypt"),
+    passport = require("passport");
 
+var MySQLStore = require('express-mysql-session')(session);
+var LocalStrategy = require('passport-local').Strategy;
 
 
 var con = mysql.createConnection({
@@ -13,29 +16,52 @@ var con = mysql.createConnection({
     password: "LxWTeuWU92S1ZfvX",
     database: "sandraf1"
 });
+var options = {
+    host: "mysql.agh.edu.pl",
+    user: "sandraf1",
+    password: "LxWTeuWU92S1ZfvX",
+    database: "sandraf1"
+};
+
+var sessionStore = new MySQLStore(options);
 
 app.use(session({
-    secret: 'secret',
-    resave: true,
+    secret: 'bhgycfyxrtyfyluyhcdawetsdtykg6vufyse7u',
+    store: sessionStore,
+    resave: false,
     saveUninitialized: true
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user_id, done) {
+    done(null, user_id);
+});
+
+passport.deserializeUser(function (user_id, done) {
+    done(null, user_id);
+});
+
+
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+},
+    function (req, email, password, done) {
+           profileLogin(req.body.user, req.body, done);
+        }
+    ));
+
 app.use(function (req, res, next) {
-    res.locals.currentUser = req.user;
+    res.locals.currentUserID = req.user;
     next();
 });
 
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
-    // con.query("CREATE TABLE doctors (id INT PRIMARY KEY AUTO_INCREMENT, specialization VARCHAR(255), name Varchar(255), surname VARCHAR(255), email VARCHAR(255), password VARCHAR(255))", function(err,result){
-    //     if (err){
-    //         console.log(err);
-    //     }
-    //     else{
-    //         console.log("TABLE CREATED");
-    //     }
-    // })
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -45,66 +71,69 @@ app.set("view engine", "ejs");
 app.get("/", function (req, res) {
     res.render("landing");
 });
+
 app.get("/login", function (req, res) {
     res.render("login");
 });
-app.post("/login", function (req, res) {
-    var user = [req.body.email, req.body.password];
-    console.log(req.body)
-    if (req.body.user === 'doctor') {
-        console.log("doctor");
-        con.query('SELECT * FROM doctors WHERE email = ? AND password = ? ', user, function (err, result, fields) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                if (result.length > 0) {
-                    req.session.loggedin = true;
-                    req.session.username = req.body.email;
-                    res.redirect('/calendar');
-                }
-                else {
-                    res.send("Incorrect")
-                }
-            }
-        });
-    } else {
-        console.log("patinet");
-        con.query('SELECT * FROM patients WHERE email = ? AND password = ? ', user, function (err, result, fields) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                if (result.length > 0) {
-                    req.session.loggedin = true;
-                    req.session.username = req.body.email;
-                    res.redirect('/calendar');
-                }
-                else {
-                    res.send("Incorrect")
-                }
-            }
-        });
-    }
 
-});
+app.post('/login',
+    passport.authenticate('local', {
+        successRedirect: '/calendar',
+        failureRedirect: '/login'
+    })
+);
+
 app.get("/signup", function (req, res) {
     res.render("signup");
 });
 app.get("/registerdoc", function (req, res) {
     res.render("registerdoc");
-})
+});
+
+app.post("/registerdoc", function (req, res) {
+
+
+    var confirmedPass = req.body.password2;
+    con.query("SELECT email FROM doctors WHERE email = ?", [req.body.email], function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (result.length > 0) {
+                return res.render("registerdoc", {
+                    message: "This email is already used!"
+                });
+            } else if (req.body.password !== confirmedPass) {
+                return res.render("registerdoc", {
+                    message: "Passwords do not match!"
+                });
+            } else {
+
+                bcrypt.hash(req.body.password, 8, function (err, hash) {
+                    var doctor = [req.body.specialization, req.body.name, req.body.surname, req.body.email, hash];
+                    con.query('INSERT INTO doctors (specialization, name, surname, email, password) VALUES (?, ?, ?, ?, ?)', doctor, function (err) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log("added new patinent");
+                            res.redirect("/calendar");
+                        }
+                    });
+                });
+            }
+        }
+    });
+});
+
 app.get("/registerpat", function (req, res) {
-    res.render("registerpat", {message: ""});
+    res.render("registerpat", { message: "" });
 });
 app.post("/registerpat", function (req, res) {
-    var patient = [req.body.name, req.body.surname, req.body.email, req.body.password];
     var confirmedPass = req.body.password2;
     con.query("SELECT email FROM patients WHERE email = ?", [req.body.email], function (err, result) {
         if (err) {
             console.log(err);
         } else {
-            if (result.length > 0 ) {
+            if (result.length > 0) {
                 return res.render("registerpat", {
                     message: "This email is already used!"
                 });
@@ -113,22 +142,69 @@ app.post("/registerpat", function (req, res) {
                     message: "Passwords do not match!"
                 });
             } else {
-                con.query('INSERT INTO patients (name, surname, email, password) VALUES (?, ?, ?, ?)', patient, function (err) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log("added new patinent");
-                        res.redirect("/calendar");
-                    }
+                bcrypt.hash(req.body.password, 10, function (err, hash) {
+                    var patient = [req.body.name, req.body.surname, req.body.email, hash];
+                    con.query('INSERT INTO patients (name, surname, email, password) VALUES (?, ?, ?, ?)', patient, function (err) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log("added new patinent");
+                            res.redirect("/calendar");
+                        }
+                    });
                 });
             }
         }
     });
 });
 
-    app.get("/calendar", function (req, res) {
+app.get("/logout", function (req, res) {
+    req.logOut();
+    req.session.destroy();
+    res.redirect("/calendar");
+});
+
+app.get("/calendar", function (req, res) {
     res.render("index");
 });
+
+app.get('/patient', isLoggedin, function (req, res) {
+    res.render("patient");
+    req.logOut;
+})
+
+function isLoggedin(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/");
+}
+
+function profileLogin(type, requestBody, done) {
+    var answer = 'SELECT * FROM ' + type + 's WHERE email = ?';
+    con.query(answer, requestBody.email, function (err, result, fields) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            if (result.length > 0) {
+                bcrypt.compare(requestBody.password, result[0].password, function (err, compare) {
+                    if (compare === true) {
+                        return done(null, {user_id : 9});
+                    }
+                    else {
+                        return done(null,false);
+
+                    }
+                });
+            }
+            else {
+                return done(null,false);
+            }
+        }
+    });
+}
+
 app.listen(3000, function () {
     console.log("Server connected");
 });
