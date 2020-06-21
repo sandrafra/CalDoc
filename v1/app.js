f12917d897b77886b2753a79400a824b45a3d56a
sandrafra <s.k.fraczek@gmail.com>
2020-06-21 12:17:31 +0200

    add port to mysql connection

--- v1/app.js
+++ v1/app.js
@@ -21,6 +21,7 @@ require("dotenv").config();
 
 var con = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: '3306',
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DATABASE
